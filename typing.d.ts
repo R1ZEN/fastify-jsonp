e2134c4fdc461f034cc417b465c1ac1f88bf0a84
Pavel Bely <poul-white@yandex.ru>
2022-10-29 17:59:11 +0300

    feat: typing update

--- typing.d.ts
+++ typing.d.ts
@@ -1,5 +1,11 @@
+import { FastifyPluginCallback } from 'fastify'
+
 declare module 'fastify' {
   interface FastifyReply {
     jsonp: (obj: number | string | null | Record<string, any>, options?: { callbackName: string }) => FastifyReply;
   }
 }
+
+export const fastifyJsonp: FastifyPluginCallback
+
+export default fastifyJsonp
